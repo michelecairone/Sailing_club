ec7324331f2e830fc89734a7e84d7a02515279d9
Michele Cairone <81637583+michelecairone@users.noreply.github.com>
2022-02-24 19:06:27 +0100

    Delete javaDoc directory

--- javaDoc/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"progettofinale.sailingclub"}];updateSearchResults();
--- javaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"progettofinale.sailingclub","l":"progettofinale.sailingclub"},{"m":"progettofinale.sailingclub","l":"progettofinale.sailingclub.communication"},{"m":"progettofinale.sailingclub","l":"progettofinale.sailingclub.controller"}];updateSearchResults();
